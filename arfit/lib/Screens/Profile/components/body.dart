4dee978009735c20702485ad05a5e97de49d5e36
Ritika Poddar <rp477@cornell.edu>
2022-05-06 11:12:38 -0400

    save for pulling changes

--- arfit/lib/Screens/Profile/components/body.dart
+++ arfit/lib/Screens/Profile/components/body.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:arfit/Screens/ActiveChallenges/active_challenges.dart';
+import 'package:arfit/Screens/BrowseChallenges/browse_challenges.dart';
 import 'package:arfit/Screens/Profile/components/background.dart';
-//import 'package:arfit/Screens/PopUpScreens/send_to.dart' show SendTo;
 import 'package:arfit/authentication_service.dart';
 import 'package:arfit/components/alert_widget.dart';
 import 'package:arfit/components/rounded_button.dart';
@@ -19,35 +20,114 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; // Total height and width of screen
 
+    CollectionReference userChallenges =
+        FirebaseFirestore.instance.collection('userChallenges');
+
     final FirebaseAuth auth = FirebaseAuth.instance;
     final String? userEmail = auth.currentUser?.email;
 
-    //CollectionReference users = FirebaseFirestore.instance.collection('users');
-
-    // users.doc(documentId).get();
+    Future<Map> getName() async {
+      CollectionReference users =
+          FirebaseFirestore.instance.collection('users');
+      DocumentSnapshot userDocument = await users.doc(userEmail).get();
+      return userDocument.data() as Map;
+    }
 
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          // StreamBuilder<QuerySnapshot>(
+          //   stream: userChallenges.snapshots(),
+          //   builder:
+          //       (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          //     if (snapshot.hasError) {
+          //       return Text('Something went wrong');
+          //     }
+
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return Text("Loading");
+          //     }
+          //     return ListView(
+          //       scrollDirection: Axis.vertical,
+          //       shrinkWrap: true,
+          //       children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          //         Map<String, dynamic> data =
+          //             document.data()! as Map<String, dynamic>;
+          //         return ListTile(
+          //           title: Text(data['receiver']),
+          //           subtitle: Text(data['challengeId']),
+          //         );
+          //       }).toList(),
+          //     );
+          //   },
+          // ),
           SizedBox(width: double.infinity),
-          Text(
-            "PROFILE PAGE",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
+          FutureBuilder(
+            future: getName(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return Column(
+                  children: [
+                    Text(
+                      "Hi " + snapshot.data['name'] + "!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: size.height * 0.04,
+                    ),
+                    Image.network(
+                      snapshot.data['photo'],
+                      height: size.height * 0.2,
+                    )
+                  ],
+                );
+              }
+            },
+            // child: Text(
+            //   "WELCOME!",
+            //   style: TextStyle(
+            //     fontWeight: FontWeight.bold,
+            //     fontSize: 24,
+            //   ),
+            // ),
           ),
           SizedBox(
             height: size.height * 0.04,
           ),
-          SvgPicture.asset(
-            "assets/icons/signup-fitness.svg",
-            height: size.height * 0.4,
+          // SvgPicture.asset(
+          //   "assets/icons/signup-fitness.svg",
+          //   height: size.height * 0.4,
+          // ),
+          RoundedButton(
+            text: "BROWSE CHALLENGES",
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BrowseChallenges()));
+            },
           ),
-          SizedBox(
-            height: size.height * 0.03,
+          RoundedButton(
+            text: "ACTIVE CHALLENGES",
+            press: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ActiveChallenges()));
+            },
+            color: Colors.green,
           ),
+          // SizedBox(
+          //   height: size.height * 0.01,
+          // ),
           RoundedButton(
             text: "SIGN OUT",
             press: () {
@@ -61,92 +141,35 @@ class Body extends StatelessWidget {
                 ),
               );
             },
+            color: Colors.red,
           ),
-          RoundedButton(
-            text: "Try this",
-            press: () {
-              showDialog(
-                context: context,
-                builder: (_) => SimpleDialog(
-                  title: Text('Send to a Friend'),
-                  children: <Widget>[UserInformation()],
-                ),
-              );
-            },
-          ),
+          // RoundedButton(
+          //   text: "Try this",
+          //   press: () {
+          //     showDialog(
+          //       context: context,
+          //       builder: (_) => AlertWidget(
+          //           title: "Testing New Alert",
+          //           caption: "Does this work?",
+          //           actions: [
+          //             TextButton(
+          //               onPressed: () {},
+          //               child: Text("Accept"),
+          //             ),
+          //             TextButton(
+          //               onPressed: () {},
+          //               child: Text("Reject"),
+          //             ),
+          //           ]),
+          //     );
+          //   },
+          // ),
+          // AlertDialog(
+          //   title: Text("Accept?"),
+          //   content: Text("Do you accept?"),
+          // ),
         ],
       ),
     );
-  }
-}
-
-/*class SimpleDialogItem extends StatelessWidget {
-  const SimpleDialogItem({Key? key})
-      : super(key: key);
-
- 
-  List<String> text = UserInformation();
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> name_Options = [];
-    for (String name in text){
-      name_Options.add(SimpleDialogOption(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start: 16.0),
-            child: Text(name),
-          ),
-        ],
-      ),
-    );
-    );
-    }
-    return name_Options;
-  }
-}*/
-
-class UserInformation extends StatefulWidget {
-  @override
-  _UserInformationState createState() => _UserInformationState();
-}
-
-class _UserInformationState extends State<UserInformation> {
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('users').snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-            height: 200,
-            width: 200,
-            child: StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return ListView(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            print(data['name']);
-            return ListTile(
-              title: Text(data['name']),
-            );
-          }).toList(),
-        );
-      },
-    ));
   }
 }
